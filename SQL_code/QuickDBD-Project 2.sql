fa8190af803ee8ddee454fd1e4f7eb5d39ed53b2
DarylPinto26 <darylpinto26@gmail.com>
2023-09-04 17:54:13 -0400

    SQL code

--- SQL_code/QuickDBD-Project 2.sql
+++ SQL_code/QuickDBD-Project 2.sql
@@ -4,49 +4,37 @@
 
 
 CREATE TABLE "campaign" (
-    "cf_id" INT   NOT NULL,
-    "contact_id" INT   NOT NULL,
+    "cf_id" INTEGER NOT NULL PRIMARY KEY,
+    "contact_id" INTEGER NOT NULL,
     "company_name" VARCHAR(50)   NOT NULL,
     "description" VARCHAR(200)   NOT NULL,
     "goal" FLOAT   NOT NULL,
     "pledged" FLOAT   NOT NULL,
     "outcome" VARCHAR(50)   NOT NULL,
-    "backers_count" INT   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
     "country" VARCHAR(50)   NOT NULL,
     "currency" VARCHAR(50)   NOT NULL,
     "lauched_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
     "category_id" VARCHAR(50)   NOT NULL,
-    "subcategory_id" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
+    "subcategory_id" VARCHAR(50)   NOT NULL  
 );
 
 CREATE TABLE "contacts" (
-    "contact_id" INT   NOT NULL,
+    "contact_id" INTEGER   NOT NULL PRIMARY KEY,
     "first_name" VARCHAR(50)   NOT NULL,
     "last_name" VARCHAR(50)   NOT NULL,
-    "email" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "contact_id"
-     )
+    "email" VARCHAR(50)   NOT NULL
 );
 
 CREATE TABLE "category" (
-    "category_id" VARCHAR(50)   NOT NULL,
-    "category" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_category" PRIMARY KEY (
-        "category_id"
-     )
+    "category_id" VARCHAR(50)   NOT NULL PRIMARY KEY,
+    "category" VARCHAR(50)   NOT NULL
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(50)   NOT NULL,
-    "subcategory" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
+    "subcategory_id" VARCHAR(50)   NOT NULL PRIMARY KEY,
+    "subcategory" VARCHAR(50)   NOT NULL
 );
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
@@ -58,3 +46,8 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
+
+SELECT * FROM campaign
+SELECT * FROM contacts
+SELECT * FROM category
+SELECT * FROM subcategory
